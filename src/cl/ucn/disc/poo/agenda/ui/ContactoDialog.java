b02eacef4f51d035adf4cf1ad9dd78f3883672ba
godiecl <mika.cl@gmail.com>
2023-12-07 17:46:58 -0300

    feat: add the layout

--- src/cl/ucn/disc/poo/agenda/ui/ContactoDialog.java
+++ src/cl/ucn/disc/poo/agenda/ui/ContactoDialog.java
@@ -4,6 +4,8 @@
 
 package cl.ucn.disc.poo.agenda.ui;
 
+import cl.ucn.disc.poo.agenda.domain.Contacto;
+
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
@@ -19,6 +21,14 @@ public class ContactoDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
+    private JTextField textRut;
+    private JTextField textNombre;
+    private JTextField textApellido;
+    private JTextField textCalle;
+    private JTextField textCiudad;
+    private JTextField textCodigoPostal;
+    private JTextField textCodigoArea;
+    private JTextField textNumero;
 
     private AgendaModel agendaModel;
 
@@ -27,37 +37,60 @@ public class ContactoDialog extends JDialog {
      *
      * @param agendaModel to use.
      */
-    public ContactoDialog(AgendaModel agendaModel) {
+    public ContactoDialog(final AgendaModel agendaModel) {
         this.agendaModel = agendaModel;
+        // titulo del formulario
+        this.setTitle("Información de Contacto");
+        // contenido del formulario
         this.setContentPane(contentPane);
+        // formulario bloque el acceso a la ventana de atras
         this.setModal(true);
+        // boton por defecto es el cancelar
         this.getRootPane()
-            .setDefaultButton(buttonOK);
+                .setDefaultButton(buttonCancel);
 
         // call onOK() on ESCAPE
-        buttonOK.addActionListener(e -> onOK());
+        this.buttonOK.addActionListener(e -> this.onOK());
 
         // call onCancel() on ESCAPE
-        buttonCancel.addActionListener(e -> onCancel());
+        this.buttonCancel.addActionListener(e -> this.onCancel());
 
         // call onCancel() when cross is clicked
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
                 onCancel();
             }
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        this.contentPane.registerKeyboardAction(e -> this.onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
     /**
      * Al momento de presionar OK.
      */
     private void onOK() {
-        // add your code here
-        dispose();
+
+        // cajita de rut, nombre y apellido con datos
+        if (!this.textRut.getText().trim().isEmpty()
+                && !this.textNombre.getText().trim().isEmpty()
+                && !this.textApellido.getText().trim().isEmpty()) {
+
+            // construccion del contacto
+            Contacto contacto = Contacto.builder()
+                    .rut(this.textRut.getText().trim())
+                    .nombre(this.textNombre.getText().trim())
+                    .apellido(this.textApellido.getText().trim())
+                    .build();
+
+            // agrego el contacto al model
+            this.agendaModel.add(contacto);
+
+            // cierro la ventana
+            dispose();
+        }
+
     }
 
     /**
